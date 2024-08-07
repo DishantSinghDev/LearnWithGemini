72b7b098af323c66bae612e96c24f8a9508df0c2
DishantSinghDev <dishupandey57@gmail.com>
2024-08-07 10:42:19 +0000

    new updates

--- components/shared/customModal.tsx
+++ components/shared/customModal.tsx
@@ -1,16 +1,10 @@
 import Modal from "@/components/shared/modal";
-import {
-  useState,
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useMemo,
-} from "react";
+import { useState, Dispatch, SetStateAction, useCallback, useMemo } from "react";
 
 const CustomModal = ({
-  children,
   showCustomModal,
   setShowCustomModal,
+  children,
 }: {
   children: React.ReactNode;
   showCustomModal: boolean;
@@ -23,21 +17,20 @@ const CustomModal = ({
   );
 };
 
-export function useCustomModal({children}: {children: React.ReactNode}) {
+export function useCustomModal({ children }: { children: React.ReactNode }) {
   const [showCustomModal, setShowCustomModal] = useState(false);
 
-  const CustomModalCallback = useCallback(() => {
-    return (
-      <CustomModal
-        children={children}
-        showCustomModal={showCustomModal}
-        setShowCustomModal={setShowCustomModal}
-      />
-    );
-  }, [children, showCustomModal, setShowCustomModal]);
+  const CustomModalCallback = useCallback(() => (
+    <CustomModal
+      showCustomModal={showCustomModal}
+      setShowCustomModal={setShowCustomModal}
+    >
+      {children}
+    </CustomModal>
+  ), [children, showCustomModal, setShowCustomModal]);
 
-  return useMemo(
-    () => ({ setShowCustomModal, CustomModal: CustomModalCallback }),
-    [setShowCustomModal, CustomModalCallback],
-  );
+  return useMemo(() => ({
+    setShowCustomModal,
+    CustomModal: CustomModalCallback,
+  }), [setShowCustomModal, CustomModalCallback]);
 }
